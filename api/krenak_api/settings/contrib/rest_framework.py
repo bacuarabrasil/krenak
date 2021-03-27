123cbd1474df2193cf747314dc6ea67436f47e43
icaro <icarooliv2@gmail.com>
2021-03-27 17:42:00 -0300

    implements jwt login
    Co-authored-by: Guilherme Siqueira <guilhersiqueira@gmail.com>
    Co-authored-by: Alandouglas Mendes <alandgmendes@gmail.com>
    Co-authored-by: Djorkaeff Alexandre <djorkaeff.unb@gmail.com>

--- api/krenak_api/settings/contrib/rest_framework.py
+++ api/krenak_api/settings/contrib/rest_framework.py
@@ -1,9 +1,15 @@
+from datetime import timedelta
+
+from ..django import SECRET_KEY
 from ..environment import env
 
 
 REST_FRAMEWORK = {
     "COERCE_DECIMAL_TO_STRING": False,
-    "DEFAULT_AUTHENTICATION_CLASSES": ("krenak_api.apps.accounts.api.authentication.CustomSessionAuthentication",),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "krenak_api.apps.accounts.api.authentication.CustomSessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "DEFAULT_PARSER_CLASSES": ("rest_framework.parsers.JSONParser",),
@@ -11,4 +17,27 @@ REST_FRAMEWORK = {
         "KRENAK_API_DEFAULT_RENDERER_CLASSES", default=("rest_framework.renderers.JSONRenderer",)
     ),
     "PAGE_SIZE": 100,
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5000),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "UPDATE_LAST_LOGIN": False,
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "VERIFYING_KEY": None,
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "JTI_CLAIM": "jti",
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
 }
